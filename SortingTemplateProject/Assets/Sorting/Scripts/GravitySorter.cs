b11d55de84e9f8d19ede75e5414a36007be9ed14
Lala-banners <lala.l.banno@hotmail.com>
2021-02-04 15:07:56 +1100

    added cute background music

---
+++ SortingTemplateProject/Assets/Sorting/Scripts/GravitySorter.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using Sorting;
+using System;
+
+public class GravitySorter : BaseSorter
+{
+    //
+
+    protected override IEnumerator Sort()
+    {
+        int nodeCount = nodes.Length; //how many nodes there are
+        Node tempNode; //nodes = visual bar 
+
+       
+        return null;
+    }
+}
